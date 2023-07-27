6dbc05ff558c55b30df8a9c97edbcf6c6ec599e4
AbhayMalhan <121591196+AbhayMalhan@users.noreply.github.com>
2023-07-27 19:59:07 +0530

    Create GenericAction.java

---
+++ src/main/java/actions/GenericAction.java
@@ -0,0 +1,49 @@
+package Actions;
+
+import Base.BaseClass;
+import PageObjects.GenericObjects;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+
+public class GenericAction extends BaseClass {
+
+
+    GenericObjects genericObjects=new GenericObjects();
+
+    public void initialize() throws IOException {
+        driver = initializeDriver();
+        driver.manage().window().maximize();
+        driver.get(prop.getProperty("url"));
+        WebElement companyTitle=genericObjects.companyTitle();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Boolean result=companyTitle.isDisplayed();
+        Assert.assertTrue(result);
+    }
+
+    public void enterUsername(String username) {
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        WebElement usernameField= genericObjects.usernameField();
+        usernameField.sendKeys(username);
+
+
+    }
+
+    public void enterPassword(String password) {
+        WebElement passwordField= genericObjects.passwordField();
+        passwordField.sendKeys(password);
+
+    }
+
+    public void clickOnSignInBtn() {
+        WebElement signInBtn= genericObjects.signInBtn();
+        signInBtn.click();
+
+    }
+
+
+}
