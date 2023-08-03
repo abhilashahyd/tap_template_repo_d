422801ed0e2fc1eeed551da7231b2be2ce984d04
AbhayMalhan <121591196+AbhayMalhan@users.noreply.github.com>
2023-08-03 17:41:43 +0530

    Update BaseClass.java

--- src/main/java/Base/BaseClass.java
+++ src/main/java/Base/BaseClass.java
@@ -15,6 +15,22 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+package Actions;
+
+import Base.BaseClass;
+import PageObjects.EmployeeObjects;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
 
