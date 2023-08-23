9824b45ea7aa8b529bee4dc6766107d016ed9836
AbhayMalhan <121591196+AbhayMalhan@users.noreply.github.com>
2023-08-23 17:32:01 +0530

    Update BaseClass.java

--- src/main/java/Base/BaseClass.java
+++ src/main/java/Base/BaseClass.java
@@ -58,7 +58,7 @@ public class BaseClass {
             // IE code
         }
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
     }
 
