4c1edd950f8f65b6e8d3ad320577f8ae9f45181d
AbhayMalhan <121591196+AbhayMalhan@users.noreply.github.com>
2023-08-24 13:48:11 +0530

    Create Utilities.java

---
+++ src/main/java/utils/Utilities.java
@@ -0,0 +1,16 @@
+package utils;
+
+import java.util.Date;
+import org.openqa.selenium.TakesScreenshot;
+import java.io.File;
+import java.io.FileInputStream;
+public class Utilities{
+  public void takeScreenShot() throws IOException {
+
+        Date currentDate = new Date();
+        String screenShotFileName = currentDate.toString().replace(" ", "_").replace(":", "_");
+
+        File screenshotFile = (((TakesScreenshot) driver)).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotFile, new File("src\\test\\resources\\testData\\screenShot\\" + screenShotFileName + ".png"));
+    }
+}
